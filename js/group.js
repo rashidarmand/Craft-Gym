9a3c7501a203063fea3750fa64d03458c149c803
Rashid H Armand <rashidharmand@gmail.com>
2018-04-10 11:30:55 -0400

    more JS clean up

--- js/group.js
+++ js/group.js
@@ -8,33 +8,7 @@ class GymLeaders {
         return this.group;
     }
 
-    get(name) {
-        for (let element of this.party) {
-            if (name.toLowerCase() === element.name){
-                return element;
-                
-            }
-        }
-    }
-
-    getMove(moveName) {
-        for (let element of this.party) {
-            
-            if(moveName.toLowerCase() === element.moveName.toLowerCase()){
-                return element;
-            }
-
-        }
-
-    }
-
 }
 
-
-
-
-myGymLeaders.group.push(ivan.all())
+myGymLeaders.group.push(ivan.all() 
 myGymLeaders.group.push(professor.all)
-
-
-// console.log(professor)
